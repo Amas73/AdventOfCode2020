b4a73d302e79f195e2a858dacface5c12f43ee17
Amas73 <44020327+Amas73@users.noreply.github.com>
2020-12-03 17:38:32 +1000

    Add files via upload

--- AoC_day02.py
+++ AoC_day02.py
@@ -6,12 +6,12 @@ for row in in_data:
   rule=row[0].split('-')
   l_cnt = 0
   for p in row[2]:
-    if p==row[1][:-1]: l_cnt = l_cnt +1
-  if l_cnt>=int(rule[0]) and l_cnt<=int(rule[1]): p_cnt = p_cnt + 1
+    if p==row[1][:-1]: l_cnt+=1
+  if l_cnt>=int(rule[0]) and l_cnt<=int(rule[1]): p_cnt+=1
 print ('Part 1# of passwords: ' + str(p_cnt))
 
 p_cnt = 0
 for row in in_data:
   rule=row[0].split('-')
-  if (row[2][int(rule[0])-1]==row[1][:-1])^(row[2][int(rule[1])-1]==row[1][:-1]): p_cnt = p_cnt + 1
+  if (row[2][int(rule[0])-1]==row[1][:-1])^(row[2][int(rule[1])-1]==row[1][:-1]): p_cnt += 1
 print ('Part 2# of passwords: ' + str(p_cnt))

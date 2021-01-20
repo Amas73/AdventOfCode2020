13c4797a241ce87a859db43b20ad74f0a88a34f4
Amas73 <44020327+Amas73@users.noreply.github.com>
2021-01-20 10:14:30 +1000

    Add files via upload

--- AoC_day12.py
+++ AoC_day12.py
@@ -10,19 +10,46 @@ y,x = 0,0
 dir={'N':[-1,0],'E':[0,1],'S':[1,0],'W':[0,-1],'F':[0,1]}
 
 for i in rawdata:
-  if i[0] in ['L','R']:
-    if i[0] == 'L':
+  d = i[0]
+  n = int(i[1:])
+  if d in ['L','R']:
+    if d == 'L':
       a=-1
     else:
       a=1
-    angle = math.radians(a*int(i[1:]))
+    angle = math.radians(a*n)
     yt = round(math.sin(angle) * (dir['F'][1]) + math.cos(angle) * (dir['F'][0]))
     xt = round(math.cos(angle) * (dir['F'][1]) - math.sin(angle) * (dir['F'][0]))
     dir['F']=[yt,xt]
   else:
-    d = i[0]
-    n = int(i[1:])
     y += dir[d][0]*n
     x += dir[d][1]*n
 
 print ('Part 1:',abs(y)+abs(x))
+
+
+y,x = 0,0
+dir['F']=[-1,10]
+
+for i in rawdata:
+  d = i[0]
+  n = int(i[1:])
+  if d in ['L','R']:
+    if d == 'L':
+      a=-1
+    else:
+      a=1
+    angle = math.radians(a*n)
+    yt = round(math.sin(angle) * (dir['F'][1]) + math.cos(angle) * (dir['F'][0]))
+    xt = round(math.cos(angle) * (dir['F'][1]) - math.sin(angle) * (dir['F'][0]))
+    dir['F']=[yt,xt]
+  elif d == 'F':
+    y += dir[d][0]*n
+    x += dir[d][1]*n
+  else:
+    yt = (dir[d][0]*n) + dir['F'][0]
+    xt = (dir[d][1]*n) + dir['F'][1]
+    dir['F']=[yt,xt]
+
+print ('Part 2:',abs(y)+abs(x))
+print(y,x)
